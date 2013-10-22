98b22ebae17435987eb4c341a38165be3048d8f8
crsacramento <gekka.bijin.7@gmail.com>
2013-10-22 18:26:16 +0100

    Buy is done. Validate is done but doesnt yet verify if there is another one validated yet. Buslist isnt done yet. Have to do better error output

--- root.rb
+++ root.rb
@@ -1,6 +1,10 @@
 require 'rubygems'
 require 'sinatra'
 require 'data_mapper'
+require 'dm-core'
+require 'dm-timestamps'
+require 'dm-validations'
+require 'dm-migrations'
 require 'date'
 require 'json'
 
@@ -25,7 +29,6 @@ class User
   }
 
   property :login,                  String, {
-    key: true,
     unique: true,
     required: true,
     length: 5..50
@@ -58,7 +61,6 @@ class Ticket
   property :id, Serial
 
   property :bus_mac_address,        String, {
-    required: true,
     format: /^([0-9A-F]{2}:){5}[0-9A-F]{2}$/
   }
 
@@ -79,8 +81,9 @@ DataMapper.finalize.auto_upgrade!
 disable :protection
 
 # methods START
-# :_login/:_password/:_name/:_num/:_type/:_val
 post '/register' do
+    # {"name":"Diogo Teixeira","password":"password","login":"diogo","num":"12345678","type":"Visa","val":"1382310000"}
+
     params = JSON.parse(request.body.read, {symbolize_names: true})
     puts params
     user = User.new
@@ -98,53 +101,112 @@ post '/register' do
         {"error" => false}.to_json
     else
         # answer error
-        {"error" => true}.to_json
+        # {"error" => true}.to_json
+        user.errors.each do |error|
+            puts error
+        end
+        user.errors.to_json
     end
 end
 
-get '/user/:login' do |login|
-# test method, displays user info
+get '/tickets/:login' do |login|
+# test method, lists non-validated tickets
     user = User.first(:login => login)
-{user:
-        {
-        name: user.name,
-        password: user.password,
-        login: user.login,
-        num: user.credit_card_num,
-        type: user.credit_card_type,
-        val: user.credit_card_val
-         }
-    }.to_json
+    user.tickets.all(:validated_at => nil).to_json
 end
 
 post '/buy' do
-    # params = login, num_tickets15m, num_tickets30m, num_tickets60m
-    
+    # {login":"diogo","num_tickets15m":1,"num_tickets30m":1,"num_tickets60m":0}"
+    params = JSON.parse(request.body.read, {symbolize_names: true})
+   
     # find client
+    user = User.first(:login => params[:login])
+
+    if !user
+        return {'error' => 'User nonexistent'}.to_json
+    end
     
+    if((params[:num_tickets15m].to_i + params[:num_tickets30m].to_i + params[:num_tickets60m].to_i) == 0)
+        return {'error' => 'Bought 0 tickets.'}.to_json
+    end
 
     $i = 0
-    while $i < params[:num_tickets15m] do
+    while $i < params[:num_tickets15m].to_i do
         # new ticket of type 15 mins
-    end
-    $i = 0
-    while $i < params[:num_tickets30m] do
-        # new ticket of type 30 mins
-    end
-   $i = 0
-    while $i < params[:num_tickets60m] do
-        # new ticket of type 60 mins
+        ticket = user.tickets.new(:validity_time => 15)
+        bool = ticket.save
+        if !bool
+            return ticket.errors.to_json
+        end
+        $i = $i + 1
     end
 
+    $i = 0
+    
+    while $i < params[:num_tickets30m].to_i do
+        # new ticket of type 30 mins
+        ticket = user.tickets.new(:validity_time => 30)
+        bool = ticket.save
+        if !bool
+           return ticket.errors.to_json
+        end
+        $i = $i + 1
+    end
+    
+    $i = 0
+    
+    while $i < params[:num_tickets60m].to_i do
+        # new ticket of type 60 mins
+        ticket = user.tickets.new(:validity_time => 60)
+        bool = ticket.save
+        if !bool
+            ticket.errors.to_json
+        end
+        $i = $i + 1
+    end
+
+    # check if user qualifies for extra ticket
+    if((params[:num_tickets15m].to_i + params[:num_tickets30m].to_i + params[:num_tickets60m].to_i) >= 10)
+        if params[:num_tickets15m].to_i > 0
+            # free 15m ticket
+            user.tickets.create(:validity_time => 15)
+        elsif params[:num_tickets30m].to_i > 0
+            # free 30m ticket
+            user.tickets.create(:validity_time => 30)
+        elsif params[:num_tickets60m].to_i > 0
+            # free 60m ticket
+            user.tickets.create(:validity_time => 60)
+        end
+    end
+
+    user.tickets.all(:validated_at => nil).to_json
 end
 
 post '/validate' do
-    # params = mac_address, ticket_id
+    # {"bus":"00:00:00:00:00:00","ticket":1}
+    params = JSON.parse(request.body.read, {symbolize_names: true})
 
+    ticket = Ticket.get(params[:ticket].to_i)
+    if ticket.validated_at == nil and ticket.bus_mac_address == nil
+        ticket.bus_mac_address = params[:bus_mac_address]
+        ticket.validated_at = Time.now
+        bool = ticket.save
+        if !bool
+            return ticket.errors.to_json
+        end
+    
+        ticket = Ticket.get(params[:ticket].to_i)
+        return ticket.to_json
+    else
+        return ticket.errors.to_json
+    end
 end
 
 post '/buslist' do
-    # params = mac_address
+    #{"bus":"00:00:00:00:00:00"}
+    params = JSON.parse(request.body.read, {symbolize_names: true})
+    tickets = Ticket.all(:bus_mac_address => params[:bus]) & Ticket.all(:validated_at.lte => (Time.now - 90*60).to_s)
+    tickets.to_json
 end
 
 # methods END
